dadd0e31f323f0e8826c5a10101fa5c1e64b772e
sunny <test@qq.com>
2019-09-22 20:33:36 +0800

    首次提交项目

---
+++ myProvider/src/main/java/com/offcn/service/impl/HelloServiceImpl.java
@@ -0,0 +1,13 @@
+package com.offcn.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.offcn.service.HelloService;
+
+ @Service
+ public class HelloServiceImpl implements HelloService {
+    @Override
+    public String speakHello(String name) {
+        return "你好："+name+"欢迎来优就业学习";
+    }
+}
+ 
